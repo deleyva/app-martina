4574c29eda616b9465bb816c8856a1d6f46dc235
deleyva <jesuslopezdeleyva@gmail.com>
2025-03-25 18:44:41 +0100

    allowed host added

--- config/settings/local.py
+++ config/settings/local.py
@@ -14,7 +14,12 @@ SECRET_KEY = env(
     default="JtAoPdUhrpItD1KYbA58Ebo3bDj6rtJQQzx8eiyuKpn4DIjXtGiOQkqyIfaOfynS",
 )
 # https://docs.djangoproject.com/en/dev/ref/settings/#allowed-hosts
-ALLOWED_HOSTS = ["localhost", "0.0.0.0", "127.0.0.1"]  # noqa: S104
+ALLOWED_HOSTS = [
+    "localhost",
+    "0.0.0.0",
+    "127.0.0.1",
+    "apps.iesmartinabescos.es",
+]  # noqa: S104
 
 # CACHES
 # ------------------------------------------------------------------------------
